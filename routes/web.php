5f7c69d1fb125c6846d1a61105e9a2332e4a27be
Rizki Ramadhan <rizki@nadyne.net>
2024-08-12 15:50:14 +0700

    praktik route pada saat webinar

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('hello', function() {
+    return 'ini laravel saya!';
 });
+
+Route::get('tampilan/{nilai1}', function($nilai1) {
+    return 'nilai saya adalah : ' . $nilai1;
+});
+
+Route::get('tambah/{nilai1}/{nilai2}', function($nilai1, $nilai2) {
+    return 'nilai penjumlahan adalah : ' . $nilai1 + $nilai2;
+});
+
+Route::get('kurang/{nilai1}/{nilai2}', function($nilai1, $nilai2) {
+    return 'nilai pengurangan adalah : ' . $nilai1 - $nilai2;
+});
+
+Route::get('kali/{nilai1}/{nilai2}', function($nilai1, $nilai2) {
+    return 'nilai perkalian adalah : ' . $nilai1 * $nilai2;
+});
+
+Route::get('bagi/{nilai1}/{nilai2}', function($nilai1, $nilai2) {
+    return 'nilai pembagian adalah : ' . $nilai1 / $nilai2;
+});
+
